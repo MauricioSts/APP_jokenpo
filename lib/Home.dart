79ed6603a2214a5d7fab3473d190e18a4bc830c8
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-03-13 15:19:22 -0300

    Finalizando o segundo app

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +9,52 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _imageApp = AssetImage("images/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo";
+
+  void _opcaoSelecionada(String escolhaUsuario) {
+    var opcoes = ["pedra", "papel", "tesoura"];
+
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+    print("miha escolha:" + "$escolhaUsuario");
+    print("escolha app:" + escolhaApp);
+
+    switch (escolhaApp) {
+      case "pedra":
+        setState(() {
+          this._imageApp = AssetImage("images/pedra.png");
+        });
+        break;
+
+      case "papel":
+        setState(() {
+          this._imageApp = AssetImage("images/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          this._imageApp = AssetImage("images/tesoura.png");
+        });
+        break;
+    }
+
+    if ((escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+        (escolhaUsuario == "papel" && escolhaApp == "pedra")) {
+      setState(() {
+        this._mensagem = "Ganheiiii";
+      });
+    } else if ((escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+        (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+        (escolhaApp == "papel" && escolhaUsuario == "pedra")) {
+      this._mensagem = "Voce perdeu!!!";
+    } else {
+      this._mensagem = "Empateeeeeeee";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,25 +74,34 @@ class _HomeState extends State<Home> {
               "Escolha do App:",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            GestureDetector(
-              onTap: () {
-                print("olaaaa");
-              },
-              child: Image.asset("images/padrao.png", width: 150),
-            ),
-
+            Image(image: this._imageApp),
             SizedBox(height: 20),
             Text(
-              "Varivavel derrota ou vitora",
+              _mensagem,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Image.asset("images/pedra.png", width: 100),
-                Image.asset("images/papel.png", width: 100),
-                Image.asset("images/tesoura.png", width: 100),
+                GestureDetector(
+                  onTap: () {
+                    _opcaoSelecionada("pedra");
+                  },
+                  child: Image.asset("images/pedra.png", width: 100),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _opcaoSelecionada("papel");
+                  },
+                  child: Image.asset("images/papel.png", width: 100),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _opcaoSelecionada("tesoura");
+                  },
+                  child: Image.asset("images/tesoura.png", width: 100),
+                ),
               ],
             ),
           ],
